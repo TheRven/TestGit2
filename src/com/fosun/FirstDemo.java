851da35efffae1c980384fd50b66bf2c7f610e13
baymin <jys971029@163.com>
2018-12-13 08:42:07 +0800

    now update this project

--- src/com/fosun/FirstDemo.java
+++ src/com/fosun/FirstDemo.java
@@ -1,5 +1,8 @@
 package com.fosun;
 
+/**
+ * 现在对项目进行升级
+ */
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("hello github");
---
+++ src/com/fosun/SecondDemo.java
@@ -0,0 +1,7 @@
+package com.fosun;
+
+public class SecondDemo {
+    public static void main(String[] args) {
+        System.out.println("this is second demo");
+    }
+}
